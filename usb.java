ceeb823918132fc16f696d75c1f991e780aa9a7d
pengalangazhang <pengzhang1999@gmail.com>
2019-07-02 16:10:43 -0400

    Rewrote write function, added read function

--- usb.java
+++ usb.java
@@ -1,5 +1,6 @@
 import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.nio.IntBuffer;
 
 import org.usb4java.BufferUtils;
@@ -15,84 +16,82 @@ import org.usb4java.LibUsb;
 import org.usb4java.LibUsbException;
 
 public class usb {
-
-	private static byte END_POINT_IN = (byte) 0x81;
-	private static byte END_POINT_OUT = (byte) 0x01;
 	
 	private final static byte USB_DIR_IN = (byte) 0x80;
 	private final static byte USB_DIR_OUT = (byte) 0x00;
 	
 	private final static short HIKEY_VENDORID = (short) 0x18D1;
-	private final static short HIKEY_PRODUCTID = (short) 0x4EE7;
+	private final static short ACCESSORY_PRODUCTID = (short) 0x2D01;
 	
-	private final static short ACC_PRODUCTID = (short) 0x2D01;
+	private final static int STANDARD_INTERFACE = 0;
+	private final static int ADB_INTERFACE = 1;
+	
+	private static final long TIMEOUT = 5000;
 	
 	private static Context context;
 	private static Device device;
 	private static DeviceHandle handle;
-	private static boolean isShutdown;
+	private static byte endpointIn;
+	private static byte endpointOut;
+	
+	/*
+	 * To start accessory mode, HiKey must have libusbk/libusb-win32 driver installed; winusb - error 3
+	 */
 	
 	public static void main(String[] args) {
-		isShutdown = false;
-		Runtime.getRuntime().addShutdownHook(new Thread() {
-			public void run() {
-				isShutdown = true;
-			}
-		});
 		try {
 			init();
-			int result = setupAccessory("Google","Hikey960", "Description", "1.0", "http://www.mycompany.com", "SerialNumber");
-			if ( result != LibUsb.SUCCESS ) {
-				throw new LibUsbException("Unable to setup Accessory", result);
+			DeviceDescriptor desc = new DeviceDescriptor();
+			LibUsb.getDeviceDescriptor(device, desc);		
+			if ( Short.compare(desc.idProduct(), ACCESSORY_PRODUCTID) == 0 && Short.compare(desc.idVendor(), HIKEY_VENDORID) == 0 ) {
+				System.out.println("Hikey is already in accessory mode");
 			}
-			device = findDevice(HIKEY_VENDORID);
-			LibUsb.claimInterface(handle, 0);
-			LibUsb.setConfiguration(handle, 1);
-			try {
-				Thread.sleep(20000);
+			else {
+				int result = setupAccessory("Google","Hikey960", "Description", "1.0", "http://www.mycompany.com", "SerialNumber");
+				if ( result != LibUsb.SUCCESS ) {
+					throw new LibUsbException("Unable to setup Accessory", result);
+				}
+				//wait for accessory mode
+				try {
+					Thread.sleep(TIMEOUT);
+				}
+				catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				init();
 			}
-			catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			getEndPointAddress(); //TODO Check if it works
-			write(handle); //TODO Rewrite method
+			
+			getEndPointAddress();
+			LibUsb.releaseInterface(handle, 1);
+			LibUsb.close(handle);
+			LibUsb.exit(context);
 		}
 		catch (Exception e) {
 			e.printStackTrace();
 		}
-		finally {
-			LibUsb.releaseInterface(handle, 0);
-			LibUsb.resetDevice(handle);
-			LibUsb.close(handle);
-			LibUsb.exit(context);
-		}
 	}
 	
-	public static void write(DeviceHandle handle) {
-		int data = 0;
-		ByteBuffer buffer = BufferUtils.allocateByteBuffer(1);
-		while (!isShutdown) {
-			buffer.rewind();
-			buffer.put((byte) data);
-			IntBuffer transferred = BufferUtils.allocateIntBuffer();
-			int result = LibUsb.bulkTransfer(handle, END_POINT_OUT, buffer, transferred, 0);
-			if ( result != LibUsb.SUCCESS ) {
-				throw new LibUsbException("Bulk Transfer failure", result);
-			}
-			System.out.println(transferred.get() + " bytes sent to device");
-			if ( data < 255 ) {
-				data = data + 1;
-			}
-			else {
-				data = 0;
-			}
-			try {
-				Thread.sleep(50);
-			}
-			catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+	public static void write(DeviceHandle handle, byte[] data) {
+		ByteBuffer buffer = BufferUtils.allocateByteBuffer(data.length);
+		buffer.rewind();
+		buffer.put(data);
+		IntBuffer transferred = BufferUtils.allocateIntBuffer();
+		int result = LibUsb.bulkTransfer(handle, endpointOut, buffer, transferred, TIMEOUT);
+		if ( result != LibUsb.SUCCESS ) {
+			throw new LibUsbException("Bulk Transfer failure", result);
 		}
+		System.out.println(transferred.get() + " bytes sent to device");
+	}
+	
+	public static ByteBuffer read(DeviceHandle handle, int size) {
+		ByteBuffer buffer = BufferUtils.allocateByteBuffer(size).order(ByteOrder.LITTLE_ENDIAN);
+		IntBuffer transferred = BufferUtils.allocateIntBuffer();
+		int result = LibUsb.bulkTransfer(handle, endpointIn, buffer, transferred, TIMEOUT);
+		if ( result  != LibUsb.SUCCESS ) {
+			throw new LibUsbException("Unable to read data", result);
+		}
+		System.out.println(transferred.get() + " bytes read from device");
+		return buffer;
 	}
 	
 	private static void init() {
@@ -103,22 +102,24 @@ public class usb {
 		}
 		
 		device = findDevice(HIKEY_VENDORID);
-		DeviceDescriptor desc = new DeviceDescriptor();
-		LibUsb.getDeviceDescriptor(device, desc);
-		System.out.println(desc.dump());
 		handle = new DeviceHandle();
+		
+		DeviceDescriptor desc = new DeviceDescriptor();
+		LibUsb.getDeviceDescriptor(device, desc);		
 		
 		result = LibUsb.open(device, handle);
 		if ( result < 0 ) {		
 			throw new LibUsbException("Unable to open USB device", result);
 		}
-		System.out.println("Successfully opened");
+		System.out.println("USB successfully opened");
 		
-		result = LibUsb.claimInterface(handle, 0);
+		
+		int iface = Short.compare(ACCESSORY_PRODUCTID, desc.idProduct()) == 0 ? ADB_INTERFACE : STANDARD_INTERFACE;
+		result = LibUsb.claimInterface(handle, iface);
 		if ( result != LibUsb.SUCCESS ) {
 			throw new LibUsbException("Unable to claim interface", result);
 		}
-		System.out.println("Successfully claimed");
+		System.out.println("Interface successfully claimed");
 	}
 	
 	private static Device findDevice(short vendorId) {
@@ -142,8 +143,8 @@ public class usb {
 		}
 		finally {
 			LibUsb.freeDeviceList(list, false);
+			list = null;
 		}
-		
 		return null;
 	}
 	
@@ -153,10 +154,10 @@ public class usb {
 		EndpointDescriptor[] endPoints = (configDescriptor.iface()[0]).altsetting()[0].endpoint();
 		for(EndpointDescriptor endPoint : endPoints) {
 			if ( DescriptorUtils.getDirectionName(endPoint.bEndpointAddress()) == "IN" ) {
-				END_POINT_IN = (byte)(endPoint.bEndpointAddress() % 0xff);
+				endpointIn = (byte)(endPoint.bEndpointAddress() % 0xff);
 			}
 			if ( DescriptorUtils.getDirectionName(endPoint.bEndpointAddress()) == "OUT" ) {
-				END_POINT_OUT = (byte)(endPoint.bEndpointAddress() % 0xff);
+				endpointOut = (byte)(endPoint.bEndpointAddress() % 0xff);
 			}
 		}
 	}
@@ -175,9 +176,14 @@ public class usb {
 		response = transferAccessoryDataPacket(serial, (short) 5);
 		//setup handshake packet
 		response = transferSetupPacket((short) 0, (byte)(USB_DIR_OUT | LibUsb.REQUEST_TYPE_VENDOR), (byte) 53);
-		System.out.println("Successfully put device in accessory mode, bytes: " + response);
+		System.out.println("Successfully put device in accessory mode");
 		
 		LibUsb.releaseInterface(handle, 0);
+		LibUsb.close(handle);
+		LibUsb.exit(context);
+		
+		handle = null;
+		context = null;
 		
 		return response;
 	}
@@ -187,7 +193,6 @@ public class usb {
 		byte[] fill = null;
 		try {
 			fill = param.getBytes("UTF-8");
-			System.out.println(new String(fill, "UTF-8"));
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
@@ -195,24 +200,22 @@ public class usb {
 		for (int i = 0; i < byteArray.length; i++) {
 			if ( i < fill.length ) {
 				byteArray[i] = fill[i];
-				System.out.print(fill[i]);
 			}
 			else {
 				byteArray[i] = 0;
-				System.out.print('0');
 			}
 		}
-		System.out.println();
 		byteArray[fill.length] = 0;
 		ByteBuffer data = BufferUtils.allocateByteBuffer(byteArray.length);
 		data.put(byteArray);
+		
 		final byte bRequest = (byte) 52;
 		final short wValue = 0;
 		final long timeout = 0;
+		
 		data.rewind();
 		response = LibUsb.controlTransfer(handle, (byte) (USB_DIR_OUT | LibUsb.REQUEST_TYPE_VENDOR), bRequest, wValue, index, data, timeout);
 		if ( response < 0 ) {
-			System.out.println(response);
 			throw new LibUsbException("Unable to control transfer.", response);
 		}
 		return response;
@@ -230,7 +233,6 @@ public class usb {
 		
 		data.rewind();
 		response = LibUsb.controlTransfer(handle, requestType, request, wValue, wIndex, data, timeout);
-		System.out.println(LibUsb.strError(response));
 		if ( response < 0 ) {
 			throw new LibUsbException("Unable to transfer setup packet.", response);
 		}
